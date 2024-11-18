e2f56ed17000bcd5a248a7509f70c47d237aa48b
sprikymd <mdspriky@gmail.com>
2024-11-18 19:24:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
+    SESSION_ID: process.env.SESSION_ID || 'W2wiHShA#cq6qZGEdlOXPotO8fL1FRcUHJBRpxtn6IPHFVT39QXw', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || 'mongodb+srv://elisaqueen525:1234@cluster0.qf2vrdx.mongodb.net/',// Enter Your MongoDB URL
     OWNER_NUMBER: process.env.OWNER_NUMBER || '94758900210',    // Enter Your Owner Number
 };
